a85a9a06c58e10bc02f1b18f033f7fed2e8bf05d
Yannick Bättig <yannick.baettig@gmail.com>
2022-11-23 22:03:43 +0100

    remove Intro template

--- components/IndexPage.tsx
+++ components/IndexPage.tsx
@@ -1,21 +1,20 @@
-import Container from 'components/BlogContainer'
-import BlogHeader from 'components/BlogHeader'
-import Layout from 'components/BlogLayout'
-import HeroPost from 'components/HeroPost'
-import MoreStories from 'components/MoreStories'
-import IntroTemplate from 'intro-template'
-import type { Post, Settings } from 'lib/sanity.queries'
-import Head from 'next/head'
+import Container from "components/BlogContainer";
+import BlogHeader from "components/BlogHeader";
+import Layout from "components/BlogLayout";
+import HeroPost from "components/HeroPost";
+import MoreStories from "components/MoreStories";
+import type { Post, Settings } from "lib/sanity.queries";
+import Head from "next/head";
 
 export default function IndexPage(props: {
-  preview?: boolean
-  loading?: boolean
-  posts: Post[]
-  settings: Settings
+  preview?: boolean;
+  loading?: boolean;
+  posts: Post[];
+  settings: Settings;
 }) {
-  const { preview, loading, posts, settings } = props
-  const [heroPost, ...morePosts] = posts
-  const { title = 'Blog.' } = settings
+  const { preview, loading, posts, settings } = props;
+  const [heroPost, ...morePosts] = posts;
+  const { title = "Blog." } = settings;
 
   return (
     <>
@@ -37,8 +36,7 @@ export default function IndexPage(props: {
           )}
           {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
-        <IntroTemplate />
       </Layout>
     </>
-  )
+  );
 }
